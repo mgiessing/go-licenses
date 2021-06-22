de4059c73f432b4a0435606912b2256472b9c223
Yuan Gong <gongyuan94@gmail.com>
2021-06-22 00:27:18 +0000

    chore: clean up root command

--- v2/cmd/root.go
+++ v2/cmd/root.go
@@ -27,16 +27,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-licenses",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "go-licenses -- a license workflows CLI tool",
+	Long: `go-licenses is a CLI tool for Go that automates license workflows.
+It helps find licenses of your dependencies and comply with them.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -59,8 +52,4 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-licenses.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
